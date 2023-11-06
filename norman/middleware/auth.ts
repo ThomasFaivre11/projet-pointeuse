9461764e4b1900392f25b6753a34a8bd3f0ba10b
Michael Bardou <michael@60fps.fr>
2023-11-06 14:48:51 +0100

    FIX middleware

--- norman/middleware/auth.ts
+++ norman/middleware/auth.ts
@@ -1,13 +1,13 @@
-import check_token from '../composables/useAuth'
+import useAuth from '../composables/useAuth';
 
-export default defineNuxtRouteMiddleware(() => {
-	setTimeout(() => {
-		console.log('middleware');
-		const token = check_token()
-		if (token){
-			window.location.href = `http://localhost:3000/dashboard`
-		}else {
-			window.location.href = `http://localhost:3000/`
-		}
-	}, 0.00001);
+export default defineNuxtRouteMiddleware((to, from) => {
+	const token = useAuth().check_token();
+
+	if (!token) {
+		return navigateTo('/');
+	}
+
+	if (to.path !== '/') {
+		return navigateTo('/');
+	}
 });
