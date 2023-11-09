0e117dff588f52d7e2dc98191fb17cc2ca3ae440
Michael Bardou <michael@60fps.fr>
2023-11-09 15:05:03 +0100

    team

--- norman/components/crud/Teams.vue
+++ norman/components/crud/Teams.vue
@@ -1,7 +1,52 @@
-<script></script>
+<script>
+import TeamUserCard from '~/components/TeamUserCard.vue';
+
+export default {
+	components: {
+		TeamUserCard,
+	},
+};
+</script>
 
 <template>
-	<div></div>
+	<div class="team-container">
+		<h3>Nom de la team</h3>
+		<div class="manager">nom du manager</div>
+		<div class="team">
+			<!-- <TeamUserCard v-for="worker in workers_data" :name_user="worker.data.username" :status_user="worker.data.status" /> -->
+		</div>
+	</div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.team-container {
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+	gap: 20px;
+	@include desktop {
+		gap: 30px;
+	}
+
+	h3 {
+		@include SuisseIntl;
+		font-size: 30rem;
+		font-weight: 500;
+		color: $darkblue;
+	}
+
+	.manager {
+		@include SuisseIntl;
+		font-size: 20rem;
+		font-weight: 500;
+		color: $darkblue;
+	}
+
+	.team {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 20px;
+		margin: 0 30px;
+	}
+}
+</style>
